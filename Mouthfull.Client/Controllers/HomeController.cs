4f2caff31dbf30cbd7d6841e3fc665c96e380e41
Gledinburgh <gledinburgh@gmail.com>
2021-05-22 14:56:57 -0500

    Successfully connected to Mouthfull API via HttpClient

--- Mouthfull.Client/Controllers/HomeController.cs
+++ Mouthfull.Client/Controllers/HomeController.cs
@@ -2,18 +2,48 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Mouthfull.Client.Models;
+using Newtonsoft.Json;
 
 namespace Mouthfull.Client.Controllers
 {
+  [Route("[controller]")]
   public class HomeController : Controller
   {
-    public IActionResult Index()
+    private IConfiguration _configuration;
+
+    public HomeController(IConfiguration configuration)
     {
-      return View();
+      _configuration = configuration;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+      var client = new HttpClient();
+      var response = await client.GetAsync($"{_configuration["Services:mouthfullapi"]}");
+      List<object> result = null;
+
+      if (response.IsSuccessStatusCode)
+      {
+        System.Console.WriteLine("====================================");
+        System.Console.WriteLine(response.Content.ReadAsStringAsync());
+        result = JsonConvert.DeserializeObject<List<object>>(await response.Content.ReadAsStringAsync());
+        ViewBag.response = result;
+        return View("Index");
+      }
+      // if (!response.IsSuccessStatusCode)
+      // {
+      //   result = JsonConvert.DeserializeObject<List<string>>(await response.Content.ReadAsStringAsync());
+      //   ViewBag.response = result;
+      //   return View("ResponseSuccess");
+      // }
+      return View("Index");
     }
   }
+
 }
