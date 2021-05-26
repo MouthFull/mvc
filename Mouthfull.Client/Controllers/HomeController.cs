b8c5be633b7efbf8c523d33a48434ff786f0e0a3
Phuc Bao Pham <phucbao.pham@revature.net>
2021-05-25 20:48:32 -0500

    added recipesummary

--- Mouthfull.Client/Controllers/HomeController.cs
+++ Mouthfull.Client/Controllers/HomeController.cs
@@ -6,45 +6,68 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Mouthfull.Client.Singletons;
 using Mouthfull.Client.Models;
+using Mouthfull.Domain.Models;
+using System.Net.Http.Json;
 
 namespace Mouthfull.Client.Controllers
 {
-  [Route("[controller]/[action]")]
-  public class HomeController : Controller
-  {
-    private static IConfiguration _configuration;
-    private ClientSingleton _clientSingleton;
-    private HomeViewModel _homeViewModel;
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        private static IConfiguration _configuration;
+        private ClientSingleton _clientSingleton;
+        private HomeViewModel _homeViewModel;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-    public HomeController(IConfiguration configuration, HomeViewModel homeViewModel)
-    {
-      _configuration = configuration;
-      _clientSingleton = ClientSingleton.Instance(configuration);
-      _homeViewModel = homeViewModel;
-    }
-    [HttpGet]
-    public IActionResult Index(HomeViewModel home)
-    {
-      home.LoadDummyData();
-      return View("Index", home);
-    }
-    [HttpPost]
-    public async Task<IActionResult> SearchRecipes(HomeViewModel home)
-    {
-      System.Console.WriteLine("=====================>" + home.Input);
-      var ingredients = home.Input;
-      System.Console.WriteLine("=====================>" + ingredients);
-      var response = await _clientSingleton.GetRecipies(ingredients);
-      home.LoadRecipes(response);
-      return View("Index", home);
-    }
-    public async Task<IActionResult> History() 
+        public HomeController(IConfiguration configuration, HomeViewModel homeViewModel, IHttpClientFactory httpClientFactory)
         {
-           // var response = await _clientSingleton.GetRecipies("");
+            _configuration = configuration;
+            _clientSingleton = ClientSingleton.Instance(configuration);
+            _homeViewModel = homeViewModel;
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index(HomeViewModel home)
+        {
+            home.LoadDummyData();
+            return View("Index", home);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SearchRecipes(HomeViewModel home)
+        {
+            System.Console.WriteLine("=====================>" + home.Input);
+            var ingredients = home.Input;
+            System.Console.WriteLine("=====================>" + ingredients);
+            var response = await _clientSingleton.GetRecipies(ingredients);
+            home.LoadRecipes(response);
+            return View("Index", home);
+        }
+        public async Task<IActionResult> History()
+        {
+            // var response = await _clientSingleton.GetRecipies("");
             HomeViewModel home = new HomeViewModel();
             home.LoadDummyData();
             var recipes = home.Recipes;
             return View("History", recipes);
         }
-  }
+        public async Task<IActionResult> RecipeSummary(int id)
+        {
+            string endpoints = "";
+            var httpclient = _httpClientFactory.CreateClient();
+            var response = await httpclient.GetAsync($"{endpoints}{id}");
+            Recipe result = new Recipe();
+
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadFromJsonAsync<Recipe>();
+
+                return View("RecipeSummary", result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
 }
